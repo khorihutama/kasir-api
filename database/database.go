bcc06ce4729690df87f446cb9bbd857b9fb50319
khorihutama <khori.qq@gmail.com>
2026-01-29 23:13:06 +0700

    fix: remove print

--- database/database.go
+++ database/database.go
@@ -2,25 +2,20 @@ package database
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 
 	_ "github.com/lib/pq"
 )
 
 func InitDB(connectionString string) (*sql.DB, error) {
-	fmt.Println(connectionString)
 	db, err := sql.Open("postgres", connectionString)
 	if err != nil {
-		fmt.Println("here")
 		return nil, err
 	}
 
 	// Test connection
 	err = db.Ping()
 	if err != nil {
-		fmt.Println("or here")
-
 		return nil, err
 	}
 
